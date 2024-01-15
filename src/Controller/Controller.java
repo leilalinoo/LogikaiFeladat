83920a1cd7b04e7d05199dba40726b6b0a58b14d
Wendesday36 <bertazoeanna@gmail.com>
2024-01-15 10:22:35 +0100

    controller

---
+++ src/Controller/Controller.java
@@ -0,0 +1,23 @@
+package Controller;
+
+import logikaioop.Lada;
+
+public class Controller {
+
+    public static void main(String[] args) {
+        Lada a1 = new Lada("én rejtem a kincset!!", "Arany láda", false);
+        Lada a2 = new Lada("nem én rejtem a kincset...", "Ezüst láda", true);
+        Lada a3 = new Lada("az arany hazudik!!!", "Bronz láda", false);
+        vizsgalat(a2);
+    }
+    
+    
+    public static void vizsgalat(Lada a2){
+    String valasz = bekeres();
+        String v1 = "Megtaláltad a kincset!!";
+        if (!a2.eredmeny(valasz)) {
+            v1 = "Vesztettél, az ezüst ládában van a kincs :(";
+
+        }
+    }
+}
